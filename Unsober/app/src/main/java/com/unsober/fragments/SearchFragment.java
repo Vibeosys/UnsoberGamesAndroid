29f4fbe7887dc717fd5e617bf1588f8f30a5abe0
ShrinivasSH <shrinivas@vibeosys.com>
2016-07-06 19:37:46 +0530

    spinner  is added
    spinner  is added

--- Unsober/app/src/main/java/com/unsober/fragments/SearchFragment.java
+++ Unsober/app/src/main/java/com/unsober/fragments/SearchFragment.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import java.util.List;
 import com.unsober.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by akshay on 05-07-2016.
@@ -19,6 +24,27 @@ public class SearchFragment extends Fragment {
         //return super.onCreateView(inflater, container, savedInstanceState);
 
         View view = inflater.inflate(R.layout.fragment_search_advance,container,false);
+        Spinner spinner = (Spinner)view.findViewById(R.id.spinner);
+        Spinner spinner1 = (Spinner)view.findViewById(R.id.spinner1);
+        Spinner spinner2 = (Spinner)view.findViewById(R.id.spinner2);
+        Spinner spinner3 = (Spinner)view.findViewById(R.id.spinner3);
+        List<String> categories = new ArrayList<String>();
+        categories.add("Football");
+        categories.add("Table tennis ");
+        categories.add("chess");
+        categories.add("Boxing");
+        categories.add("Snooker");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        spinner1.setAdapter(dataAdapter);
+        spinner2.setAdapter(dataAdapter);
+        spinner3.setAdapter(dataAdapter);
         return view;
     }
 }
