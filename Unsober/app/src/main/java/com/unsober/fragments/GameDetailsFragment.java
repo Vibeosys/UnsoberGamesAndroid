84e407372459f09081f26883aa466c259bf3d2b1
ShrinivasSH <shrinivas@vibeosys.com>
2016-07-06 18:35:48 +0530

    search fragment screen is added
    search fragment screen is added

--- Unsober/app/src/main/java/com/unsober/fragments/GameDetailsFragment.java
+++ Unsober/app/src/main/java/com/unsober/fragments/GameDetailsFragment.java
@@ -3,10 +3,12 @@ package com.unsober.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ScrollingView;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -18,17 +20,27 @@ import com.unsober.R;
  */
 public class GameDetailsFragment extends Fragment {
     private TextView mGameDescription;
+    private ScrollingView mScrollingView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_game_details,container,false);
+      //  mScrollingView = (ScrollingView) view.findViewById(R.id.gameScrollview);
 
         mGameDescription =(TextView) view.findViewById(R.id.txtDescription);
         mGameDescription.setText(Html.fromHtml(getResources().getString(R.string.game_description)));
+        /*mGameDescription.setFocusable(true);*/
 
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+       /* super.onCreateOptionsMenu(menu, inflater);*/
+        inflater.inflate(R.menu.game_details_search,menu);
+        MenuItem menuItem = menu.findItem(R.id.gameDetails_search);
 
+
+    }
 }
--- Unsober/app/src/main/java/com/unsober/fragments/SearchFragment.java
+++ Unsober/app/src/main/java/com/unsober/fragments/SearchFragment.java
@@ -1,9 +1,24 @@
 package com.unsober.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.unsober.R;
 
 /**
  * Created by akshay on 05-07-2016.
  */
 public class SearchFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_search_advance,container,false);
+        return view;
+    }
 }
