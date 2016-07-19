4b07af998514ae9b8c4e11e674534a4cd91b7049
AkshayVGadhave <akshay@vibeosys.com>
2016-07-19 19:56:38 +0530

    add no of  views in advance search
    add no of  views in advance search

--- Unsober/app/src/main/java/com/unsober/fragments/AdvancedSearchFragment.java
+++ Unsober/app/src/main/java/com/unsober/fragments/AdvancedSearchFragment.java
@@ -1,5 +1,6 @@
 package com.unsober.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -10,11 +11,19 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.unsober.R;
 import com.unsober.adapter.ItemsListAdapter;
 import com.unsober.data.adapterdata.GameListDataDTO;
+import com.unsober.utils.NetworkUtils;
 
 import java.util.ArrayList;
 
@@ -22,6 +31,7 @@ import java.util.ArrayList;
  * Created by shrinivas on 09-07-2016.
  */
 public class AdvancedSearchFragment extends ItemListBaseFragment {
+    private static final String TAG = AdvancedSearchFragment.class.getSimpleName();
     private AdView mAdView;
     private ListView mListView;
     private String mWhereClause;
@@ -72,12 +82,17 @@ public class AdvancedSearchFragment extends ItemListBaseFragment {
     }
 
     private void callToNext(GameListDataDTO gameListDataDTO) {
-        GameDetailsFragment gameDetailsFragment = new GameDetailsFragment();
-        Bundle bundle = new Bundle();
-        bundle.putLong("itemId", gameListDataDTO.getItemId());
-        gameDetailsFragment.setArguments(bundle);
-        getFragmentManager().beginTransaction().replace(R.id.fragment_frame_lay, gameDetailsFragment).commit();
-        BaseFragment.stackFragment.push(this);
+        if (NetworkUtils.isActiveNetworkAvailable(getActivity().getApplicationContext())) {
+            uploadView(gameListDataDTO.getItemId());
+        } else {
+            GameDetailsFragment gameDetailsFragment = new GameDetailsFragment();
+            Bundle bundle = new Bundle();
+            bundle.putLong("itemId", gameListDataDTO.getItemId());
+            mDbRepository.updateViews(gameListDataDTO.getItemId());
+            gameDetailsFragment.setArguments(bundle);
+            getFragmentManager().beginTransaction().replace(R.id.fragment_frame_lay, gameDetailsFragment).commit();
+            BaseFragment.stackFragment.push(this);
+        }
     }
 
     @Override
@@ -127,6 +142,82 @@ public class AdvancedSearchFragment extends ItemListBaseFragment {
     @Override
     protected TextView getErrorView() {
         return mTxtSearchError;
+    }
+
+    protected void uploadView(long itemId) {
+        uploadJsonToServer(mSessionManager.getViewUrl(), itemId);
+    }
+
+
+    private void uploadJsonToServer(String uploadUrl,
+                                    final long itemId) {
+        RequestQueue vollyRequest = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+       /* String sendItemId = "" + itemId;
+        JSONObject json = new JSONObject();
+        try {
+            json.put("id", sendItemId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+        uploadUrl = uploadUrl + "?id=" + itemId;
+        StringRequest uploadRequest = new StringRequest(Request.Method.GET,
+                uploadUrl, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                if (response == null) {
+                    Log.e(TAG, "Error to get the data from server");
+                    return;
+                }
+                onResultReceived(itemId);
+                Log.d(TAG, "## Success in get views response");
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                onResultReceived(itemId);
+                Log.e(TAG, "## Error in get views response" + error.toString());
+            }
+        });
+        uploadRequest.setRetryPolicy(new DefaultRetryPolicy(15000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        vollyRequest.add(uploadRequest);
+    }
+
+    private void onResultReceived(long itemId) {
+        InsertUpdateView insertUpdateView = new InsertUpdateView();
+        insertUpdateView.execute(itemId);
+
+    }
+
+    private class InsertUpdateView extends AsyncTask<Long, Void, Void> {
+
+        long itemId = 0;
+
+        @Override
+        protected Void doInBackground(Long... params) {
+            itemId = params[0];
+            mDbRepository.updateViews(itemId);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            callToFragment(itemId);
+        }
+    }
+
+    private void callToFragment(long itemId) {
+        GameDetailsFragment gameDetailsFragment = new GameDetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putLong("itemId", itemId);
+        gameDetailsFragment.setArguments(bundle);
+        getFragmentManager().beginTransaction().replace(R.id.fragment_frame_lay, gameDetailsFragment).commit();
+        BaseFragment.stackFragment.push(this);
     }
 
 }
